8123b74bdc13d3ac7b5ccd1db615508f6fcdb6e6
Quangang Sheng <quangang.sheng@wz-inc.com>
2017-09-26 16:00:52 +0800

    Initial commit

---
+++ src/form.hpp
@@ -0,0 +1,12 @@
+#ifndef FORM_HPP_INCLUDED
+#define FORM_HPP_INCLUDED
+
+namespace org {
+    namespace sqg {
+
+        class form {
+        };
+    }
+}
+
+#endif // FORM_HPP_INCLUDED
---
+++ tests/test1.hpp
@@ -0,0 +1,24 @@
+#ifndef TEST1_HPP_INCLUDED
+#define TEST1_HPP_INCLUDED
+
+#if defined(__MINGW__) || defined(__CYGWIN__)
+#   include <w32api.h>
+#endif
+
+#define _WIN32_IE _WIN32_IE_IE30
+
+#include <windows.h>
+#include <commctrl.h>
+
+#define IDI_APPICON                    101
+#define IDR_MAINMENU                   102
+#define IDR_ACCELERATOR                103
+#define IDD_ABOUTDIALOG                104
+#define ID_FILE_EXIT                   40001
+#define ID_HELP_ABOUT                  40002
+
+#ifndef IDC_STATIC
+  #define IDC_STATIC                   -1
+#endif
+
+#endif // TEST1_HPP_INCLUDED
